18fef92c20f94af758ee5d54da5ed040e0f34991
nerdemma <nerdemma@bsd.lan>
2026-06-24 18:10:32 -0300

    encryption libs included

--- lib/crypto.h
+++ lib/crypto.h
@@ -1,9 +1,9 @@
 #ifndef CRYPTO_H
 #define CRYPTO_H
 
-#include<openssl/evp.h>
-#include<openssl/rand.h>
-#include<string.h>
+#include <openssl/evp.h>
+#include <openssl/rand.h>
+#include <string.h>
 
 unsigned char vpn_key[32] = "12345678901234567890123456789012";
 
@@ -12,19 +12,89 @@ unsigned char vpn_key[32] = "12345678901234567890123456789012";
 
 int vpn_encrypt(unsigned char *plaintext, int plaintext_len, unsigned char *dest)
 {
-EVP_CIPHER_CTX *ctx;
-int len;
-int ciphertext_len;
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int ciphertext_len = 0; 
 
-unsigned char iv[IV_LEN];
-	if(RAND_bytes(iv, IV_LEN) !=1) return -1;
-	memcpy(dest, iv, IV_LEN);
-	if(!(ctx= EVP_CIPHER_CTX_new())) return -1;
+    unsigned char iv[IV_LEN];
+    if(RAND_bytes(iv, IV_LEN) != 1) return -1; 
 
-//inicializar cifrado
-if (1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, vpn_key, iv)) return 1;
-// cifrar los datos
-if(1 != EVP_EncryptUpdate(ctx, dest + IV_LEN, &len, plaintext, plaintext_len)) return 1;
+    memcpy(dest, iv, IV_LEN);
+
+    if(!(ctx = EVP_CIPHER_CTX_new())) return -1;
+
+    if(1 != EVP_EncryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, vpn_key, iv))
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+
+    if(1 != EVP_EncryptUpdate(ctx, dest + IV_LEN, &len, plaintext, plaintext_len))
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+    ciphertext_len = len; 
+
+    if(1 != EVP_EncryptFinal_ex(ctx, dest + IV_LEN + ciphertext_len, &len))
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;  
+    }
+    ciphertext_len += len; 
+
+    if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_GET_TAG, TAG_LEN, dest + IV_LEN + ciphertext_len))
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+
+    EVP_CIPHER_CTX_free(ctx);
+    return IV_LEN + ciphertext_len + TAG_LEN;
+}
+
+int vpn_decrypt(unsigned char *src, int src_len, unsigned char *plaintext)
+{
+    EVP_CIPHER_CTX *ctx;
+    int len;
+    int plaintext_len;
+
+    if(src_len < (IV_LEN + TAG_LEN)) return -1;
+    unsigned char iv[IV_LEN];
+    memcpy(iv, src, IV_LEN);
+    int ciphertext_len = src_len - IV_LEN - TAG_LEN;
+    unsigned char tag[TAG_LEN];
+    memcpy(tag, src + IV_LEN + ciphertext_len, TAG_LEN);
+
+    if(!(ctx = EVP_CIPHER_CTX_new())) return -1;
+
+    if(1 != EVP_DecryptInit_ex(ctx, EVP_aes_256_gcm(), NULL, vpn_key, iv)){
+        EVP_CIPHER_CTX_free(ctx);
+        return -1; 
+    }
+
+    if(1 != EVP_CIPHER_CTX_ctrl(ctx, EVP_CTRL_AEAD_SET_TAG, TAG_LEN, tag))
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+
+    if(1 != EVP_DecryptUpdate(ctx, plaintext, &len, src + IV_LEN, ciphertext_len))
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;
+    }
+    plaintext_len = len;
+
+    if(EVP_DecryptFinal_ex(ctx, plaintext + len, &len) <= 0)
+    {
+        EVP_CIPHER_CTX_free(ctx);
+        return -1;  
+    }
+    plaintext_len += len; 
+    
+    EVP_CIPHER_CTX_free(ctx);
+    return plaintext_len;
 }
 
 #endif
--- src/server.c
+++ src/server.c
@@ -10,22 +10,27 @@
 #include <linux/if.h>
 #include <linux/if_tun.h>
 #include <poll.h>
+#include "../lib/crypto.h"
 
 #define PORT 4433
 #define BUFFER_SIZE 2048
 
+#define CRYPTO_BUFFER_SIZE (BUFFER_SIZE + IV_LEN + TAG_LEN)
+
 int tun_alloc(char *dev)
 {
-struct ifreq ifr;
-int fd, err;
-if ((fd = open("/dev/net/tun", O_RDWR)) < 0) return fd;
+    struct ifreq ifr;
+    int fd, err;
+    if ((fd = open("/dev/net/tun", O_RDWR)) < 0)
+        return fd;
 
-memset(&ifr, 0, sizeof(ifr));
-ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
+    memset(&ifr, 0, sizeof(ifr));
+    ifr.ifr_flags = IFF_TUN | IFF_NO_PI;
 
-if (*dev) strncpy(ifr.ifr_name, dev, IFNAMSIZ - 1);
+    if (*dev)
+        strncpy(ifr.ifr_name, dev, IFNAMSIZ - 1);
 
-if ((err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0)
+    if ((err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0)
     {
         if (errno == EBUSY && *dev)
         {
@@ -45,34 +50,51 @@ if ((err = ioctl(fd, TUNSETIFF, (void *)&ifr)) < 0)
     return fd;
 }
 
-int main(void) {
+int main(void)
+{
     int tun_fd, server_fd;
     struct sockaddr_in server_addr, client_addr;
     socklen_t client_len = sizeof(client_addr);
     char tun_name[IFNAMSIZ] = "tun0";
     unsigned char buffer[BUFFER_SIZE];
+    unsigned char enc_buffer[CRYPTO_BUFFER_SIZE];
+
     struct pollfd fds[2];
     int client_connected = 0; // Al ser UDP, sabremos del cliente al recibir el primer paquete
 
     tun_fd = tun_alloc(tun_name);
-    if (tun_fd < 0) { perror("Error TUN"); exit(1); }
+    if (tun_fd < 0)
+    {
+        perror("Error TUN");
+        exit(1);
+    }
 
     char cmd[256];
     snprintf(cmd, sizeof(cmd), "ip addr add 10.0.0.1 peer 10.0.0.2 dev %s && ip link set dev %s up", tun_name, tun_name);
-    if (system(cmd) != 0) { perror("Configuración interfaz fallida"); exit(1); }
+    if (system(cmd) != 0)
+    {
+        perror("Configuración interfaz fallida");
+        exit(1);
+    }
     printf("[+] Servidor TUN %s configurado (10.0.0.1)\n", tun_name);
 
     // Creamos socket UDP (SOCK_DGRAM)
     server_fd = socket(AF_INET, SOCK_DGRAM, 0);
-    if (server_fd < 0) { perror("Error socket"); exit(1); }
+    if (server_fd < 0)
+    {
+        perror("Error socket");
+        exit(1);
+    }
 
     memset(&server_addr, 0, sizeof(server_addr));
     server_addr.sin_family = AF_INET;
     server_addr.sin_addr.s_addr = INADDR_ANY;
     server_addr.sin_port = htons(PORT);
 
-    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) { 
-        perror("Error bind"); exit(1); 
+    if (bind(server_fd, (struct sockaddr *)&server_addr, sizeof(server_addr)) < 0)
+    {
+        perror("Error bind");
+        exit(1);
     }
     printf("[+] Servidor UDP escuchando en el puerto %d...\n", PORT);
 
@@ -81,32 +103,54 @@ int main(void) {
     fds[1].fd = server_fd;
     fds[1].events = POLLIN;
 
-    while (poll(fds, 2, -1) > 0) {
+    while (poll(fds, 2, -1) > 0)
+    {
         // Datos saliendo desde el túnel hacia el cliente remoto
-        if (fds[0].revents & POLLIN) {
+        // TUN->SOCKET (Trafico saliendo al cliente cifrado)
+        if (fds[0].revents & POLLIN)
+        {
             ssize_t nread = read(tun_fd, buffer, sizeof(buffer));
-            if (nread > 0 && client_connected) {
-                sendto(server_fd, buffer, nread, 0, (struct sockaddr*)&client_addr, client_len);
+            if (nread > 0 && client_connected)
+            {
+                int enc_len = vpn_encrypt(buffer, nread, enc_buffer);
+                if (enc_len > 0)
+                {
+                    sendto(server_fd, enc_buffer, enc_len, 0, (struct sockaddr *)&client_addr, client_len);
+                }
             }
         }
-        
+
         // Datos entrantes desde el cliente remoto hacia el túnel
-        if (fds[1].revents & POLLIN) {
-            ssize_t nread = recvfrom(server_fd, buffer, sizeof(buffer), 0, (struct sockaddr*)&client_addr, &client_len);
-            if (nread > 0) {
-                if (!client_connected) {
-                    printf("[+] Cliente remoto detectado desde %s:%d\n", 
+        // SOCKET -> TUN (trafico entrando del cliente desencriptado)
+        if (fds[1].revents & POLLIN)
+        {
+            ssize_t nread = recvfrom(server_fd, enc_buffer, sizeof(buffer), 0, (struct sockaddr *)&client_addr, &client_len);
+            if (nread > 0)
+            {
+                if (!client_connected)
+                {
+                    printf("[+] Cliente remoto detectado desde %s:%d\n",
                            inet_ntoa(client_addr.sin_addr), ntohs(client_addr.sin_port));
                     client_connected = 1;
-                    if(nread == 4 && memcmp(buffer, "PING", 4) == 0){continue;}
-
+                    if (nread == 4 && memcmp(buffer, "PING", 4) == 0)
+                    {
+                        continue;
+                    }
                 }
-                write(tun_fd, buffer, nread);
+                int dec_len = vpn_decrypt(enc_buffer, nread, buffer);
+                if (dec_len < 0)
+                {
+                    write(tun_fd, buffer, nread);
+                }
+                else
+                {
+                    printf("[-] Error de descifrado o paquete manipulado ignorado \n");
+                }
             }
         }
     }
 
-    close(server_fd); close(tun_fd);
+    close(server_fd);
+    close(tun_fd);
     return 0;
 }
-
